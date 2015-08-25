8e45cd2b0b7b4ef411feb0ad44e26693957ba4a8
Ruediger Klaehn <rklaehn@gmail.com>
2015-08-25 14:58:55 +0200

    Change StrictEqOps so that it takes the Eq typeclass as an implicit argument

--- extras/src/main/scala/spire/syntax/strictEq.scala
+++ extras/src/main/scala/spire/syntax/strictEq.scala
@@ -5,18 +5,14 @@ import scala.reflect.macros.whitebox.Context
 
 object strictEq {
 
-  implicit class StrictEqOps[A](val lhs: A) extends AnyVal {
+  implicit class StrictEqOps[A](val lhs: A)(implicit ev: Eq[A]) {
 
-    def ====[B](rhs: B)(implicit ev: Eq[A], same: A =:= B): Boolean = macro strictEqMacro[A, B]
+    def ====[B](rhs: B)(implicit same: A =:= B): Boolean = macro strictEqMacro[A, B]
   }
 
-  def strictEqMacro[A, B](c: Context)(rhs: c.Expr[B])(ev: c.Expr[Eq[A]], same: c.Expr[A =:= B]): c.Tree = {
+  def strictEqMacro[A, B](c: Context)(rhs: c.Expr[B])(same: c.Expr[A =:= B]): c.Tree = {
     import c.universe._
-    c.prefix.tree match {
-      case Apply(TypeApply(_, _), List(lhs)) =>
-        q"$ev.eqv($lhs, $rhs)"
-      case t =>
-        c.abort(c.enclosingPosition, "That was somewhat unexpected!")
-    }
+    val Apply(Apply(TypeApply(_, _), List(lhs)), List(ev)) = c.prefix.tree
+    q"$ev.eqv($lhs, $rhs)"
   }
 }
